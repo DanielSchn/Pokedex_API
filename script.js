7eb43ec5f81e99289b9d8eb4011fb0c668f2668d
DanielSchn <d.schneider@dsc-mail>
2023-11-22 12:35:11 +0100

    load big card by click

--- script.js
+++ script.js
@@ -19,11 +19,11 @@ const typeColors = {
     "dragon": "#4F66A9",
     "dark": "#463E3E",
     "fairy": "#D282CD"
-  };
+};
 
 
 async function loadPokemon() {
-    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    let randomNumber = Math.floor(Math.random() * 300) + 1;
     let url = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`; // Can use Number or Name. 4 is charmander
     let response = await fetch(url);
     currentPokemon = await response.json();
@@ -37,7 +37,7 @@ function renderPokemon() {
     document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['official-artwork']['front_shiny'];
     getTypes();
     document.getElementById('pokemonHeight').innerHTML = `Height: ${currentPokemon['height'] / 10} m`;
-    document.getElementById('pokemonWeigth').innerHTML = `Weight: ${currentPokemon['weight'] / 10} kg`;
+    document.getElementById('pokemonWeight').innerHTML = `Weight: ${currentPokemon['weight'] / 10} kg`;
     getStats();
 }
 
@@ -46,12 +46,11 @@ function getTypes() {
     let typesHtml = '';
     let types = currentPokemon['types'];
     for (let j = 0; j < types.length; j++) {
-        typesHtml += `Type ${j+1}: ${types[j]['type']['name'].charAt(0).toUpperCase() + types[j]['type']['name'].slice(1)}<br>`;    
+        typesHtml += `Type ${j + 1}: ${types[j]['type']['name'].charAt(0).toUpperCase() + types[j]['type']['name'].slice(1)}<br>`;
     }
     document.getElementById('type').innerHTML = typesHtml;
     const typeForColor = types['0']['type']['name'];
     changeBackgroundColor(typeForColor);
-    console.log(typeForColor);
 }
 
 
@@ -79,8 +78,26 @@ function changeBackgroundColor(typeForColor) {
 
 function getColorForType(type) {
     if (typeColors[type] !== undefined) {
-      return typeColors[type];
+        return typeColors[type];
     } else {
-      return "unknownColor";
+        return "unknownColor";
     }
-  }
+}
+
+
+function showPokemonCard() {
+    document.getElementById('showCardBody').classList.toggle("showCard");
+    let description = document.getElementById('descriptionPokemon');
+    let stats = document.getElementById('stats');
+    description.style.display = (description.style.display === 'none' || description.style.display === '') ? 'block' : 'none';
+    stats.style.display = (stats.style.display === 'none' || stats.style.display === '') ? 'block' : 'none';
+}
+
+// NOTIZ FÜR MICH!
+// stats.style.display = (stats.style.display === 'none' || stats.style.display === '') ? 'block' : 'none'; ist das gleiche wie eine IF / ELSE, nur schöner geschrieben und kürzer.
+//
+// if (stats.style.display === 'none' || stats.style.display === '') {
+//     stats.style.display = 'block';
+// } else {
+//     stats.style.display = 'none';
+// }
